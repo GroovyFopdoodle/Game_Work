97c3fe5f50a2b167f7b02a10cac3aa650929a849
GroovyFopdoodle <101531031+GroovyFopdoodle@users.noreply.github.com>
2022-03-13 18:59:50 -0600

    Update Game_3_Script_1_game.js

--- Game_3/Game_3_Script_1_game.js
+++ Game_3/Game_3_Script_1_game.js
@@ -1,4 +1,4 @@
-import {update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection} from './Game_3_Script_2_snake.js';
+import {update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection, score} from './Game_3_Script_2_snake.js';
 import {update as updateFood, draw as drawFood} from './Game_3_Script_4_food.js';
 import {outsideGrid} from './Game_3_Script_5_grid.js';
 
@@ -8,7 +8,7 @@ const gameBoard = document.getElementById('game-board');
 
 function main(currentTime){
     if (gameOver){
-        if (confirm('You lost. Press ok to restart')){
+        if (confirm('You lost, Final score was 'score - 1'. Press ok to restart')){
             window.location = '/';
         }
         else {window.close()}
