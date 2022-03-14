0a31ee45006a600057f967bc2c98a5a002f45763
GroovyFopdoodle <101531031+GroovyFopdoodle@users.noreply.github.com>
2022-03-13 18:44:48 -0600

    Update Game_3_Script_2_snake.js

--- Game_3/Game_3_Script_2_snake.js
+++ Game_3/Game_3_Script_2_snake.js
@@ -51,8 +51,12 @@ function equalPositions(pos1, pos2){
     return pos1.x === pos2.x && pos1.y === pos2.y
 }
 
+var score = 0
+
 function addSegments(){
     for(let i = 0; i < newSegments; i++){
+        score = score++;
+        console.log(score);
         snakeBody.push({...snakeBody[snakeBody.length-1]})
     }
 
